4e72105b90231ea133ee61ab191cea5eef79313c
Jenell Weitz <jenellweitz@yahoo.com>
2019-04-25 19:52:04 -0600

    change initials

--- initials.py
+++ initials.py
@@ -1,5 +1,4 @@
 def get_initials(fullname):
     """ Given a person's name, returns the person's initials (uppercase) """
-    name = input("What is your name?")
-    return(name) 
-print(name)
+    for char in fullname:
+
