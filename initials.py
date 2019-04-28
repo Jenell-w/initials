b4745f849843cb8720a646377992bd7dc51a34b7
Jenell Weitz <jenellweitz@yahoo.com>
2019-04-27 21:19:33 -0600

    final initials assignment

--- initials.py
+++ initials.py
@@ -1,4 +1,9 @@
 def get_initials(fullname):
     """ Given a person's name, returns the person's initials (uppercase) """
-    for char in fullname:
+    inits = ""
+    for i in fullname.split():
+        inits += i[0]
+    return "The initials of " + fullname + " are " + inits.upper() 
+ 
+
 
